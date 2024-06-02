e0f3b614216b1a1e64b0658d7b4b50055d6b8292
T-i-n-o <valentino.heubach@web.de>
2024-06-02 13:57:19 +0200

    Initial commit

---
+++ plugin/source/PluginEditor.cpp
@@ -0,0 +1,32 @@
+#include "SimpleEQ/PluginEditor.h"
+#include "SimpleEQ/PluginProcessor.h"
+
+namespace audio_plugin {
+SimpleEQEditor::SimpleEQEditor(
+    SimpleEQ &p)
+    : AudioProcessorEditor(&p), processorRef(p) {
+  juce::ignoreUnused(processorRef);
+  // Make sure that before the constructor has finished, you've set the
+  // editor's size to whatever you need it to be.
+  setSize(400, 300);
+}
+
+SimpleEQEditor::~SimpleEQEditor() {}
+
+void SimpleEQEditor::paint(juce::Graphics &g) {
+  // (Our component is opaque, so we must completely fill the background with a
+  // solid colour)
+  g.fillAll(
+      getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+
+  g.setColour(juce::Colours::white);
+  g.setFont(15.0f);
+  g.drawFittedText("Hello World!", getLocalBounds(),
+                   juce::Justification::centred, 1);
+}
+
+void SimpleEQEditor::resized() {
+  // This is generally where you'll want to lay out the positions of any
+  // subcomponents in your editor..
+}
+} // namespace audio_plugin
---
+++ test/source/AudioProcessorTest.cpp
@@ -0,0 +1,10 @@
+#include <SimpleEQ/PluginProcessor.h>
+#include <gtest/gtest.h>
+
+namespace audio_plugin_test {
+
+TEST(AudioProcessor, Foo) {
+  audio_plugin::SimpleEQ processor{};
+}
+
+} // namespace audio_plugin_test
